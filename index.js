d40e04de4c327806af542fe656c635596a48bc44
Simon Grchevski <simongrcevski@gmail.com>
2022-05-22 23:17:03 +0200

    Add scrapping for sitel

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import scrapeInfoMax  from "./infoMax.js";
 import scrapeNovaMk from "./novaMk.js";
+import scrapeSitel from "./sitel.js";
 
 const app = express();
+
+
 
 app.listen("3000", () => {
     console.log("Server running at http://localhost:3000/");
@@ -14,6 +17,10 @@ app.get("/infomax", async (_,res) => {
 
 app.get("/novamakedonija", async (_,res) => {
     res.end(await scrapeNovaMk());
+})
+
+app.get("/sitel", async (_,res) => {
+    res.end(await scrapeSitel());
 })
 
 
---
+++ sitel.js
@@ -0,0 +1,20 @@
+import cheerio from "cheerio";
+import axios from "axios";
+
+const scrapeSitel = async () => {
+    const response = {};
+    const res = await axios.get("https://sitel.com.mk/");
+    const $ = cheerio.load(res.data);
+
+    $(".views-field .field-content .image img").each(function (i) {
+        response[i] = {image:$(this).attr("src")};
+    })
+
+    $(".views-field .field-content .title-wrapper a").each(function (i) {
+        response[i] = {...response[i], content:$(this).html(), link:`https://sitel.com.mk/${$(this).attr("href")}`};
+    })
+
+     return JSON.stringify(response);
+}
+
+export default scrapeSitel;
