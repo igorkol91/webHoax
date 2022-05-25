ab20d76ba2e2f00051eff0181722e2b26111c0f7
Igor Koloski <koloski_igor@hotmail.com>
2022-05-25 23:59:19 +0200

    add

--- public/telma.js
+++ public/telma.js
@@ -6,7 +6,7 @@ const scrapeTelma = async () => {
     const response = {}
     const browser = await puppeteer.launch({
         headless: true,
-        args: ['--no-sandbox','--disable-setuid-sandbox']
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--single-process']
       })
     const [page] = await browser.pages();
 
