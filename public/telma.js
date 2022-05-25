2a293011767219e92d11a71152b138034b19db24
Igor Koloski <koloski_igor@hotmail.com>
2022-05-25 22:29:18 +0200

    Add

--- public/telma.js
+++ public/telma.js
@@ -42,7 +42,7 @@ async function autoScroll(page){
                     clearInterval(timer);
                     resolve();
                 }
-            }, 100);
+            }, 500);
         });
     });
 }
