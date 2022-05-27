e77e6e346f458a16735eebd320944adb75e297dc
Simon Grchevski <simongrcevski@gmail.com>
2022-05-27 23:39:18 +0200

    Fix bug in alfa when missing image

--- public/alfa.js
+++ public/alfa.js
@@ -7,11 +7,19 @@ const scrapeAlfa = async () => {
     const $ = cheerio.load(res.data);
 
     $(".row article h3 a").each( (i,e) => {
-        response[i] = {content:$(e).text().replace(/^\s+|\s+$|\s+(?=\s)/g, ""), link:$(e).attr("href")};
+        response[i] = {content:$(e).text(), link:$(e).attr("href")};
     })
 
-    $(".row article img").each( (i,e) => {
-        response[i] = {...response[i], image:$(e).attr("src"),source:"Alfa"};
+    $(".align-items-center .read-bg-img a").each( (i,e) => {
+        console.log($(e).children('img').length);
+        if($(e).children('img').length){
+            response[i] = {...response[i], image:$(e).children('img').attr("src"),source:"Alfa"};
+        }
+        else {
+            response[i] = {...response[i], image:"",source:"Alfa"};
+        }
+        
+        
     })
 
     return JSON.stringify(response);
